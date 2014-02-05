8b7e36372e02fd13ac05f0a26266ad70960be33e
Gerard Fowley <gerard.fowley@iqeo.net>
2014-02-05 13:14:22 -0500

    runner - command switch

--- spec/hostspec_runner_spec.rb
+++ spec/hostspec_runner_spec.rb
@@ -11,13 +11,13 @@ describe Runner do
     expect { Runner.run [], out: StringIO.new, err: StringIO.new }.to_not raise_error
   end
 
-  it 'prints usage info to stderr for no arguments' do
+  it 'prints a helpful message to stderr for no arguments' do
     error = StringIO.new
     Runner.run( [], out: StringIO.new, err: error )
     error.string.should include 'hostspec'
   end
 
-  it 'prints usage info to stderr for help (-h/--help) switch' do
+  it 'prints a helpful message to stderr for help (-h/--help) switch' do
     [['-h'],['1.2.3.4','-h'],['-h','1.2.3.4'],['1.2.3.4','-h','5.6.7.8'],['--help'],['1.2.3.4','--help'],['--help','1.2.3.4'],['1.2.3.4','--help','5.6.7.8']].each do |args|
       error = StringIO.new
       Runner.run( args, out: StringIO.new, err: error )
@@ -58,15 +58,31 @@ describe Runner do
     output.string.should eq "1.1.1.1\n1.1.1.2\n1.1.1.3\n1.1.1.4\n1.1.1.5\n2.2.2.4\n2.2.2.6\n2.2.2.8\n"
   end
 
-  it 'accepts host specs for arguments before command (-c/--cmd) switch'
+  it 'accepts host specs for arguments before command (-c/--cmd) switch' do
+    Runner.run( ['1.1.1.1','2.2.2.2','-c'   ,'#'], out: StringIO.new, err: StringIO.new ).should eq 0
+    Runner.run( ['1.1.1.1','2.2.2.2','--cmd','#'], out: StringIO.new, err: StringIO.new ).should eq 0
+  end
 
-  it 'executes the command (-c/--cmd) switch argument as a shell command'
+  it 'expects a command argument after command (-c/--cmd) switch' do
+    Runner.run( ['1.1.1.1','2.2.2.2','-c'],     out: StringIO.new, err: StringIO.new ).should_not eq 0
+    Runner.run( ['1.1.1.1','2.2.2.2','-c',nil], out: StringIO.new, err: StringIO.new ).should_not eq 0
+    Runner.run( ['1.1.1.1','2.2.2.2','-c',''],  out: StringIO.new, err: StringIO.new ).should_not eq 0
+    Runner.run( ['1.1.1.1','2.2.2.2','--cmd'],     out: StringIO.new, err: StringIO.new ).should_not eq 0
+    Runner.run( ['1.1.1.1','2.2.2.2','--cmd',nil], out: StringIO.new, err: StringIO.new ).should_not eq 0
+    Runner.run( ['1.1.1.1','2.2.2.2','--cmd',''],  out: StringIO.new, err: StringIO.new ).should_not eq 0
+  end
 
+  it 'prints a useful message to stderr upon bad spec / name resolution failure'
+  it 'prints a useful message to stderr upon missing command argument'
+  it 'prints version info to stderr for version (-v/--version) switch'
+
+  it 'executes the argument after the command (-c/--cmd) switch as a shell command'
+  it 'executes multiple arguments after the command (-c/--cmd) switch as multiple shell commands'
   it 'provides hostspec values to command via environment variables $HOSTSPEC_IP, $HOSTSPEC_MASK, $HOSTSPEC_MASKLEN, $HOSTSPEC_COUNT, $HOSTSPEC_INDEX' # do
   # output = StringIO.new
   # Runner.run( '1.1.1.1-5', 'echo $HOSTSPEC_IP, $HOSTSPEC_MASK, $HOSTSPEC_MASKLEN, $HOSTSPEC_COUNT, $HOSTSPEC_INDEX', output )
   # output.string.should eq "1.1.1.1 255.255.255.255 32 6 0\n....."
   # end
-
-
+  
+  
 end
