61b994dfb2b1d4029127089891872f0869ec3270
Gerard Fowley <gerard.fowley@iqeo.net>
2014-02-04 23:04:31 -0500

    more runner stuff

--- spec/hostspec_runner_spec.rb
+++ spec/hostspec_runner_spec.rb
@@ -4,47 +4,69 @@ require 'stringio'
 
 describe Runner do
 
-  it 'requires 2 arguments (and 2 optional streams)' do
-    expect { Runner.run                }.to raise_error
-    expect { Runner.run '1.1.1.1'      }.to raise_error
-    expect { Runner.run '1.1.1.1', nil, StringIO.new, StringIO.new }.to_not raise_error
-    expect { Runner.run '1.1.1.1', '' , StringIO.new, StringIO.new }.to_not raise_error
+  it 'expects an array of arguments' do
+    expect { Runner.run           }.to raise_error
+    expect { Runner.run nil       }.to raise_error
+    expect { Runner.run '1.1.1.1' }.to raise_error
+    expect { Runner.run [], out: StringIO.new, err: StringIO.new }.to_not raise_error
   end
 
-  it 'accepts a host spec for the first argument' do
-    Runner.run( '10.20.30.40',                nil, StringIO.new, StringIO.new ).should eq 0
-    Runner.run( '10.20.30.40/24',             nil, StringIO.new, StringIO.new ).should eq 0
-    Runner.run( '10,11.20-29.30,31-38,39.40', nil, StringIO.new, StringIO.new ).should eq 0
-    Runner.run( 'localhost',                  nil, StringIO.new, StringIO.new ).should eq 0
-    Runner.run( 'no-such-host/xyz',           nil, StringIO.new, StringIO.new ).should_not eq 0
+  it 'prints usage info to stderr for no arguments' do
+    error = StringIO.new
+    Runner.run( [], out: StringIO.new, err: error )
+    error.string.should include 'hostspec'
   end
 
-  it 'accepts multiple host specs for the first argument'
-
-  it 'lists host spec IP addresses if there is no second argument' do
-    [ nil, '', '  ' ].each do |cmd| 
-      output = StringIO.new
-      Runner.run( '1.1.1.1-5', cmd, output )
-      output.string.should eq "1.1.1.1\n1.1.1.2\n1.1.1.3\n1.1.1.4\n1.1.1.5\n"
+  it 'prints usage info to stderr for help (-h/--help) switch' do
+    [['-h'],['1.2.3.4','-h'],['-h','1.2.3.4'],['1.2.3.4','-h','5.6.7.8'],['--help'],['1.2.3.4','--help'],['--help','1.2.3.4'],['1.2.3.4','--help','5.6.7.8']].each do |args|
+      error = StringIO.new
+      Runner.run( args, out: StringIO.new, err: error )
+      error.string.should include 'hostspec'
     end
   end
 
-  context 'prints usage info' do
-
-    it 'for -h or --help switch'
-
-    it 'for no arguments'
-
+  it 'accepts single host spec for argument with no switches' do
+    Runner.run( ['10.20.30.40'],                out: StringIO.new, err: StringIO.new ).should eq 0
+    Runner.run( ['10.20.30.40/24'],             out: StringIO.new, err: StringIO.new ).should eq 0
+    Runner.run( ['10,11.20-29.30,31-38,39.40'], out: StringIO.new, err: StringIO.new ).should eq 0
+    Runner.run( ['localhost'],                  out: StringIO.new, err: StringIO.new ).should eq 0
   end
 
-  context 'executes the second argument as a shell command' do
-
-    it 'with hostspec-related values in environment variables $HOSTSPEC_IP, $HOSTSPEC_MASK, $HOSTSPEC_MASKLEN, $HOSTSPEC_COUNT, $HOSTSPEC_INDEX' # do
-      # output = StringIO.new
-      # Runner.run( '1.1.1.1-5', 'echo $HOSTSPEC_IP, $HOSTSPEC_MASK, $HOSTSPEC_MASKLEN, $HOSTSPEC_COUNT, $HOSTSPEC_INDEX', output )
-      # output.string.should eq "1.1.1.1 255.255.255.255 32 6 0\n....."
-    # end
-
+  it 'fails if single host spec argument is invalid' do
+    Runner.run( ['no-such-host/xyz'],           out: StringIO.new, err: StringIO.new ).should_not eq 0
   end
+
+  it 'accepts multiple host specs for arguments with no switches' do
+    Runner.run( ['10.20.30.40/24','11.22.33.44'],                out: StringIO.new, err: StringIO.new ).should eq 0
+    Runner.run( ['10,11.20-29.30,31-38,39.40','11.22.33.44/28'], out: StringIO.new, err: StringIO.new ).should eq 0
+    Runner.run( ['localhost','1.2.3.4/26','11.22.33.44-55'],     out: StringIO.new, err: StringIO.new ).should eq 0
+  end
+
+  it 'fails if one of multiple host specs is invalid' do
+    Runner.run( ['no-such-host/xyz','5.5.5.5'], out: StringIO.new, err: StringIO.new ).should_not eq 0
+  end
+
+  it 'lists single host spec IP addresses to stdout for no command (-c/--cmd) switch' do
+    output = StringIO.new
+    Runner.run( ['1.1.1.1-5'], out: output )
+    output.string.should eq "1.1.1.1\n1.1.1.2\n1.1.1.3\n1.1.1.4\n1.1.1.5\n"
+  end
+
+  it 'lists multiple host spec IP addresses to stdout for no command (-c/--cmd) switch' do
+    output = StringIO.new
+    Runner.run( ['1.1.1.1-5','2.2.2.4,6,8'], out: output )
+    output.string.should eq "1.1.1.1\n1.1.1.2\n1.1.1.3\n1.1.1.4\n1.1.1.5\n2.2.2.4\n2.2.2.6\n2.2.2.8\n"
+  end
+
+  it 'accepts host specs for arguments before command (-c/--cmd) switch'
+
+  it 'executes the command (-c/--cmd) switch argument as a shell command'
+
+  it 'provides hostspec values to command via environment variables $HOSTSPEC_IP, $HOSTSPEC_MASK, $HOSTSPEC_MASKLEN, $HOSTSPEC_COUNT, $HOSTSPEC_INDEX' # do
+  # output = StringIO.new
+  # Runner.run( '1.1.1.1-5', 'echo $HOSTSPEC_IP, $HOSTSPEC_MASK, $HOSTSPEC_MASKLEN, $HOSTSPEC_COUNT, $HOSTSPEC_INDEX', output )
+  # output.string.should eq "1.1.1.1 255.255.255.255 32 6 0\n....."
+  # end
+
 
 end
